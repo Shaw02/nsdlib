7944ecfc0b6693b19250815651cc34d6180d3322
shaw02 <sha_w@nifty.com>
2012-12-21 09:05:56 +0900

    version 0.23

--- src/nsc/Envelop.cpp
+++ src/nsc/Envelop.cpp
@@ -44,6 +44,7 @@ const	static	Command_Info	Command[] = {
 		{	"8",	Env_Num		},
 		{	"9",	Env_Num		},
 		{	"{",	Env_Sweep	},
+		{	"(",	Env_Sweep	},
 		{	"d",	Env_Hold	},
 		{	"D",	Env_Hold	},
 		{	"|",	Env_Loop	},
@@ -56,13 +57,18 @@ const	static	Command_Info	Command[] = {
 
 	unsigned	char	cData;
 				int		i;
-				int		Loop_Normal		= -1;
-				int		Loop_Release	= -1;
-				bool	Release			= false;
-	unsigned	int		ptEnvelop		= 1;
 
 	//------------------------------
 	//クラスの初期設定
+
+	Loop_Normal		= -1;
+	Loop_Release	= -1;
+	Release			= false;
+	ptEnvelop		= 1;
+
+	code.resize(0);
+	code.append((char)1, (char)0);
+
 
 
 	//------------------------------
@@ -74,9 +80,6 @@ const	static	Command_Info	Command[] = {
 			MML->Err(L"ブロックの開始を示す{が見つかりません。");
 		}
 	}
-
-	code.resize(0);
-	code.append((char)1, (char)0);
 
 	// } が来るまで、記述ブロック内をコンパイルする。
 	while((cData = MML->GetChar()) != '}'){
@@ -104,6 +107,7 @@ const	static	Command_Info	Command[] = {
 				break;
 
 			case(Env_Sweep):
+				sweep(MML);
 				break;
 
 			case(Env_Hold):
@@ -111,13 +115,7 @@ const	static	Command_Info	Command[] = {
 				if( (i<0) || (i>255)){
 					MML->Err(L"維持時間は、0～255の範囲で指定して下さい。");
 				}
-				while(i>15){
-					code.append((char)1, (char)0x8F);
-					i -= 16;		//15を書けば、16フレーム維持
-				ptEnvelop++;
-				}
-				code.append((char)1, (char)((i & 0x0F) | 0x80));
-				ptEnvelop++;
+				setHold(i);
 				break;
 
 			case(Env_Loop):
@@ -196,6 +194,115 @@ const	static	Command_Info	Command[] = {
 Envelop::~Envelop(void)
 {
 }
+
+//==============================================================
+//		
+//--------------------------------------------------------------
+//	●引数
+//				無し
+//	●返値
+//				無し
+//==============================================================
+void	Envelop::setHold(int length)
+{
+	while(length>15){
+		code.append((char)1, (char)0x8F);
+		ptEnvelop++;
+		length -= 16;		//15を書けば、16フレーム維持
+	}
+	code.append((char)1, (char)((length & 0x0F) | 0x80));
+	ptEnvelop++;
+}
+
+//==============================================================
+//		
+//--------------------------------------------------------------
+//	●引数
+//				無し
+//	●返値
+//				無し
+//==============================================================
+void	Envelop::sweep(MMLfile* MML)
+{
+	unsigned	char	cData;
+
+	int		iStart;
+	int		iEnd;
+	int		iLength;
+	int		iDelta;
+
+	int		now;
+	int		cnt;
+
+	int		i=0;
+	int		temp;
+
+	//--------------------------
+	//●MML読み込み
+
+	//
+	iStart = MML->GetInt();
+	if( (iStart<-64) || (iStart>127)){
+		MML->Err(L"開始点は、-64～127の範囲で指定して下さい。");
+	}
+
+	cData = MML->GetChar();
+	if(cData != ','){
+		MML->Err(L"パラメータが足りません。");
+	}
+
+	iEnd = MML->GetInt();
+	if( (iEnd<-64) || (iEnd>127)){
+		MML->Err(L"終了点は、-64～127の範囲で指定して下さい。");
+	}
+
+	cData = MML->GetChar();
+	if(cData != ','){
+		MML->Err(L"パラメータが足りません。");
+	}
+
+	iLength = MML->GetInt();
+	if( (iLength<1) || (iLength>255)){
+		MML->Err(L"長さは、1～255の範囲で指定して下さい。");
+	}
+
+	cData = MML->GetChar();
+	if((cData != ')') && (cData != '}')){
+		MML->Err(L"パラメータが足りません。");
+	}
+
+	//--------------------------
+	//●テーブル作成
+
+	iDelta = iEnd - iStart;
+
+	while(i<iLength){
+		temp = iStart + (iDelta * i) / iLength;
+		if(i == 0){
+			now = temp;
+			cnt	= 0;
+		} else if (temp != now){
+			code.append((char)1, (char)now & 0x7F);
+			ptEnvelop++;
+			if(cnt>=1){
+				setHold(cnt-1);
+			}
+			now = temp;
+			cnt	= 0;
+		} else {
+			cnt++;
+		}
+		i++;
+	}
+
+	code.append((char)1, (char)now & 0x7F);
+	ptEnvelop++;
+	if(cnt>=1){
+		setHold(cnt-1);
+	}
+
+}
+
 //==============================================================
 //		コードの取得
 //--------------------------------------------------------------
--- src/nsc/Envelop.h
+++ src/nsc/Envelop.h
@@ -11,12 +11,19 @@ class Envelop :
 {
 //メンバー変数
 private:
-	unsigned	int	m_id;
+	unsigned	int		m_id;
+				int		Loop_Normal;
+				int		Loop_Release;
+				bool	Release;
+	unsigned	int		ptEnvelop;
+
 //static	const	Command_Info	Command[];	//コマンドの情報
 
 //メンバー関数
 public:
 				Envelop(MMLfile* MML, unsigned int _id, const wchar_t _strName[] = L"==== [ Envelop ]====");
 				~Envelop(void);
+		void	setHold(int length);
+		void	sweep(MMLfile* MML);
 		void	getAsm(MusicFile* MUS);
 };
