f1cc41a0e290cb4c85e5d0c6f2df0a89fbd91042
shaw02 <sha_w@nifty.com>
2020-06-17 21:26:55 +0900

    Added script to build the nsc64.exe in src/_make.bat and _clean.bat.

--- src/_clean.bat
+++ src/_clean.bat
@@ -1,8 +1,17 @@
 
+SET PATH_BACK=%PATH%
+
 rem --- nsd.lib ---
 cd nsd
 call _clean
 cd..
+
+rem --- nsc64.exe ---
+cd nsc64\
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+MSBuild -t:Clean -p:Configuration=Release;Platform="x64"
+cd..
+SET PATH=%PATH_BACK%
 
 rem --- rom.bin ---
 cd rom
--- src/_make.bat
+++ src/_make.bat
@@ -1,4 +1,7 @@
 
+
+SET PATH_BACK=%PATH%
+
 rem --- nsd.lib ---
 cd nsd
 call _make
@@ -6,17 +9,25 @@ copy *.lib ..\..\lib\
 cd..
 
 rem --- nsc.exe ---
+rem call "C:\Program Files (x86)\Microsoft Visual Studio .NET 2003\Common7\Tools\vsvars32.bat"
 cd nsc
+rem devenv nsc.sln /build Release
 cd release
 copy *.exe ..\..\..\bin\
 cd..
 cd..
+SET PATH=%PATH_BACK%
 
-cd nsc64\x64\Release
+rem --- nsc64.exe ---
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+cd nsc64\
+MSBuild -t:Build -p:Configuration=Release;Platform="x64"
+cd x64\Release
 copy *.exe ..\..\..\..\bin\
 cd..
 cd..
 cd..
+SET PATH=%PATH_BACK%
 
 rem --- rom.bin ---
 cd rom
